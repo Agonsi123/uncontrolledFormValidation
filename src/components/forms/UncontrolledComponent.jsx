e4c6f813c558628330c6177bf9561f715f26bc9f
Udodirim L. Agonsi <88558628+Agonsi123@users.noreply.github.com>
2024-06-28 10:24:29 +0100

    Update UncontrolledComponent.jsx

--- src/components/forms/UncontrolledComponent.jsx
+++ src/components/forms/UncontrolledComponent.jsx
@@ -43,45 +43,53 @@ const handleSubmit = (e) => {
 }
 
   return (
-    <div className='bg-purple-400 w-full m-4 p-8 ml-1 shadow-lg sm:m-6'>
-        <form onSubmit={handleSubmit} className='bg-white mb-4 my-1 px-3 py-6'>
-            <div className='flex flex-col px-2 mb-4'>
-                <label className='text-black text-xs font-bold mb-2 ml-[-7.1rem] sm:ml-[-8rem] tracking-wide'>First Name</label>
-                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] bg-purple-100'
+    <div className='bg-purple-400 w-full m-4 p-8 ml-1 shadow-lg md:max-w-sm md:mx-auto'>
+        <form onSubmit={handleSubmit} className='bg-white mb-4 m-2 p-5 md:flex md:flex-wrap md:justify-between'>
+            <div className='flex flex-col px-2 md:w-1/2'>
+                <label className='text-black text-xs font-bold mb-2 ml-[-6.1rem] md:-ml-5 tracking-wide'>First Name</label>
+                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] md:text[32px] text-gray-900 bg-purple-100 '
                 type="text"
                 ref={firstNameRef} 
                 />
                 {/* My code for take home */}
-                {errors.firstname && <span style={{color:'red'}}>{errors.firstname}</span>}
+                <div className='text-[10px] -ml-24 md:-ml-7'>
+                    {errors.firstname && <span style={{color:'red'}}>{errors.firstname}</span>}
+                </div>
             </div>
-            <div className='flex flex-col px-2 mb-4'>
-                <label className='text-black text-xs font-bold mb-2 -ml-28 sm:-ml-32 tracking-wide'>Last Name</label>
-                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] bg-purple-100'
+            <div className='flex flex-col px-2 md:w-1/2'>
+                <label className='text-black text-xs font-bold mb-2 -ml-24 md:-ml-1  tracking-wide'>Last Name</label>
+                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] md:text[32px] md:ml-2 text-gray-900 bg-purple-100'
                 type="text"
                 ref={lastNameRef} 
                 />
                 {/* My code for take home */}
-                {errors.lastname && <span style={{color:'red'}}>{errors.lastname}</span>}
+                <div className='text-[10px] -ml-24 md:-ml-2'>
+                    {errors.lastname && <span style={{color:'red'}}>{errors.lastname}</span>}
+                </div>
             </div>
-            <div className='flex flex-col px-2 mb-4'>
-                <label className='text-black text-xs font-bold mb-2 -ml-36 sm:-ml-40 tracking-wide'>Email</label>
-                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] bg-purple-100' 
+            <div className='flex flex-col px-2 md:w-full'>
+                <label className='text-black text-xs font-bold mb-2 -ml-32 md:ml-[-11.5rem]  tracking-wide'>Email</label>
+                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] md:text[32px] text-gray-900 bg-purple-100' 
                 type="email" 
                 ref={emailRef} 
                 />
                 {/* My code for take home */}
-                {errors.email && <span style={{color:'red'}}>{errors.email}</span>}
+                <div className='text-[10px] -ml-28 md:-ml-44'>
+                    {errors.email && <span style={{color:'red'}}>{errors.email}</span>}
+                </div>
             </div>
-            <div className='flex flex-col px-2 mb-4'>
-                <label className='text-black text-xs font-bold mb-2 ml-[-5.3rem] sm:ml-[-6.2rem] tracking-wide'>Phone Number</label>
-                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] bg-purple-100'
+            <div className='flex flex-col px-2 md:w-full'>
+                <label className='text-black text-xs font-bold mb-2 ml-[-4.5rem] md:ml-[-8rem] tracking-wide'>Phone Number</label>
+                <input className='border-hidden rounded py-1 px-4 mb-3 text-[12px] md:text[32px] text-gray-900 bg-purple-100'
                 type="tel"
                 ref={phoneNumberRef} 
                 />
                 {/* My code for take home */}
-                {errors.phone && <span style={{color:'red'}}>{errors.phone}</span>}
+                <div className='text-[10px] ml-[-4.2rem] md:-ml-32'>
+                    {errors.phone && <span style={{color:'red'}}>{errors.phone}</span>}
+                </div>
             </div>
-            <button className='bg-purple-400 hover:bg-purple-800 text-white mb-4' type="submit">Submit</button>
+            <button className='bg-purple-400 hover:bg-purple-800 text-white mb-4 w-52 md:w-[15.5rem] md:ml-2 py-1' type="submit">Submit</button>
         </form>
     </div>
   )
